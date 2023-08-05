7836678ea63b9f49b868d9705bfcf1a3f6de7541
fotino21 <ataturhan21@gmail.com>
2023-08-05 10:25:50 +0300

    bug: instabot and instapy libraries not working

--- insta_bot/app.py
+++ insta_bot/app.py
@@ -1,4 +1,15 @@
 import streamlit as st
-import instapy as inp
+from instabot import Bot
 
 st.write("hello")
+
+username = st.text_input("Please enter your username: ")
+password = st.text_input("Please enter your password: ", type="password")
+if st.checkbox("Are you using 2-Factor Authentication on your Instagram account?"):
+    security_code = st.text_input(
+        "Please enter one of your backup codes: ", type="password"
+    )
+
+if st.button("Login"):
+    bot = Bot()
+    bot.login(username=username, password=password)
