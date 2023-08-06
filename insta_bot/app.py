5291f0ab13a6cd2ce64728f778b7a8e892f84e4f
fotino21 <ataturhan21@gmail.com>
2023-08-06 11:06:19 +0300

    config: instabot fully removed

--- insta_bot/app.py
+++ insta_bot/app.py
@@ -1,7 +1,12 @@
+import os
+import time
+import random
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 import streamlit as st
-from instabot import Bot
-
-st.write("hello")
 
 username = st.text_input("Please enter your username: ")
 password = st.text_input("Please enter your password: ", type="password")
@@ -11,5 +16,4 @@ if st.checkbox("Are you using 2-Factor Authentication on your Instagram account?
     )
 
 if st.button("Login"):
-    bot = Bot()
-    bot.login(username=username, password=password)
+    pass
