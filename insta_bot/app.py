7b7b689d4ad051ec4911eaf004e14c9d65e42668
fotino21 <ataturhan21@gmail.com>
2023-08-07 15:24:47 +0300

    refactor: webdriver wait methods replaced find element method, and click made by execute script

--- insta_bot/app.py
+++ insta_bot/app.py
@@ -10,98 +10,121 @@ from selenium.webdriver.support.ui import WebDriverWait
 import streamlit as st
 from webdriver_manager.chrome import ChromeDriverManager
 from modules.utils import add_bg_from_local, set_page_config
+from selenium.webdriver.support import expected_conditions as EC
 
-DELAY_TIME = 5
+DELAY_TIME = 2
 
 
 class HomePage:
-    def __init__(self, driver):
+    def __init__(self, driver, wait):
         self.driver = driver
         self.driver.get("https://www.instagram.com/")
+        self.wait = wait
 
     def go_to_login_page(self):
         try:
-            time.sleep(DELAY_TIME)
-            self.driver.find_element(By.XPATH, "//a[text()='Log in']").click()
+            # time.sleep(DELAY_TIME)
+            goto_login_button = wait.until(
+                EC.element_to_be_clickable((By.XPATH, "//a[text()='Log in']"))
+            )
+            self.driver.execute_script("arguments[0].click();", goto_login_button)
+            # self.driver.find_element.click()
         except:
             pass
-        return LoginPage(self.driver)
+        return LoginPage(self.driver, self.wait)
 
 
 class LoginPage:
-    def __init__(self, driver):
+    def __init__(self, driver, wait):
         self.driver = driver
+        self.wait = wait
 
     def login(self, username, password):
-        time.sleep(DELAY_TIME)
-        username_input = self.driver.find_element(
-            By.CSS_SELECTOR, "input[name='username']"
+        # time.sleep(DELAY_TIME)
+        username_input = self.wait.until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, "input[name='username']")
+            )
         )
-        password_input = self.driver.find_element(
-            By.CSS_SELECTOR, "input[name='password']"
+        password_input = self.wait.until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, "input[name='password']")
+            )
         )
         username_input.send_keys(username)
         password_input.send_keys(password)
-        login_button = self.driver.find_element(By.XPATH, "//button[@type='submit']")
+        login_button = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']"))
+        )
         self.driver.execute_script("arguments[0].click();", login_button)
 
     def close_popups(self):
         time.sleep(DELAY_TIME)
         try:
-            not_now_button = self.driver.find_element(
-                By.XPATH, '//button[text()="Not Now"]'
-            )  # close notifications pop-up
-            not_now_button.click()
-            time.sleep(DELAY_TIME)
-            not_now_button = self.driver.find_element(
-                By.XPATH, '//button[text()="Not Now"]'
-            )  # close save login info pop-up
-            not_now_button.click()
-        except:
-            pass
+            not_now_button1 = self.wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//button[text()="Not Now"]'))
+            )
+            not_now_button1.click()
+            not_now_button2 = self.wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//button[text()="Not Now"]'))
+            )
+            not_now_button2.click()
+        except Exception as e:
+            print(e)
 
     def go_to_profile_page(self, username):
-        return ProfilePage(self.driver, username)
+        return ProfilePage(self.driver, self.wait, username)
 
 
 class ProfilePage:
-    def __init__(self, driver, username):
-        time.sleep(DELAY_TIME)
+    def __init__(self, driver, wait, username):
+        # time.sleep(DELAY_TIME)
         self.driver = driver
+        self.wait = wait
         self.username = username
         self.driver.get(f"https://www.instagram.com/{username}")
 
     def go_to_followers_window(self):
-        time.sleep(DELAY_TIME)
-        followers_button = self.driver.find_element(
-            By.CSS_SELECTOR, f"a[href='/{self.username}/followers/']"
+        # time.sleep(DELAY_TIME)
+        followers_button = self.wait.until(
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, f"a[href='/{self.username}/followers/']")
+            )
         )
-        followers_button.click()
+        self.driver.execute_script("arguments[0].click();", followers_button)
         return self.driver
 
     def follow_followers(self, max_count=100):
-        time.sleep(DELAY_TIME)
-        followers_dialog = self.driver.find_element(By.XPATH, "//div[@class='_aano']")
-        visible_followers = len(self.driver.find_elements(By.TAG_NAME, "li"))
+        # time.sleep(DELAY_TIME)
+        followers_dialog = self.wait.until(
+            EC.visibility_of_element_located((By.XPATH, "//div[@class='_aano']"))
+        )
+
+        visible_followers_list = self.wait.until(
+            EC.visibility_of_any_elements_located((By.TAG_NAME, "li"))
+        )
+        visible_followers = len(visible_followers_list)
         for _ in range(max_count // visible_followers + 1):
             self.driver.execute_script(
                 "arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;",
                 followers_dialog,
             )
-            time.sleep(1)
+            # time.sleep(1)
         print("Scroll down finished")
-        follow_buttons = self.driver.find_elements(By.XPATH, "//*[text()='Follow']")
+        follow_buttons = self.wait.until(
+            EC.visibility_of_any_elements_located((By.XPATH, "//*[text()='Follow']"))
+        )
         print(f"{len(follow_buttons)} profile will be followed")
         for follow_button in follow_buttons:
             self.driver.execute_script("arguments[0].click();", follow_button)
-            time.sleep(1)
+            # time.sleep(1)
         print(f"{len(follow_buttons)} profile were followed")
 
 
 def get_driver():
     options = Options()
     options.add_argument("--no-sandbox")
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--ignore-certificate-errors")
     options.add_argument("--incognito")
@@ -135,11 +158,19 @@ def main():
     influencer_username = st.text_input(
         "Please enter the username whose followers you want to follow:"
     )
+    global DELAY_TIME
+    DELAY_TIME = st.number_input(
+        "Please enter the delay amount before opening each page:",
+        min_value=1,
+        max_value=5,
+        value=3,
+    )
     if st.button("Login"):
         try:
             container = st.empty()
             driver = get_driver()
-            home_page = HomePage(driver)
+            wait = WebDriverWait(driver, DELAY_TIME)
+            home_page = HomePage(driver, wait)
             with container:
                 st.success("Instagram home page opened")
             login_page = home_page.go_to_login_page()
